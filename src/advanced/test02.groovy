e2e0263a7a42d1d02c1cf7d52547c6599a46e979
Jing <jxw1070@student.bham.ac.uk>
2020-06-23 23:53:20 +0100

    closure

--- src/advanced/test02.groovy
+++ src/advanced/test02.groovy
@@ -1,4 +1,18 @@
 package advanced
 
-class test02 {
+def method1 (Closure clo) {
+    clo()
+    //clo.call()
 }
+method1 {{
+    println 'hahaha'
+}}
+
+def method2 (closure) {
+//    closure('jane')
+     closure.call('jane')
+}
+
+method2({
+    val->println "my name is ${val}"
+})
